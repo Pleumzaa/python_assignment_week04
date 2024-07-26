87e4d618af9ed2791828f2646620ac3a6189dc84
Kris <suwanyothinkris@gmail.com>
2024-07-26 22:09:07 +0700

    adding

---
+++ calculator.py
@@ -0,0 +1,22 @@
+def add(num1, num2):
+    sum = num1 + num2
+    return sum 
+    
+def subtract(num1, num2):
+    sum = num1 - num2
+    return sum 
+
+def multiply(num1, num2):
+    sum = num1*num2
+    return sum
+
+def divide(num1, num2):
+    sum = num1/num2
+    return sum
+
+num1 = 10
+num2 = 5
+print(add(num1,num2))
+print(subtract(num1,num2))
+print(multiply(num1,num2))
+print(divide(num1,num2))
